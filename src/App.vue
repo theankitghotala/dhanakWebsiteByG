1472af137fb06f07bc748914b6a120fd2cb59527
Aaditya Raj Anand <adityaraj912830@gmail.com>
2024-01-14 15:17:45 +0530

    14th jan 3:17 PM

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <navbar />
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js Application"/>
   <BoxCard />
 </template>
 
