f64905946fff20b672caf3e27ff592cc0ecbd770
Aaditya Raj Anand <adityaraj912830@gmail.com>
2024-01-18 22:22:00 +0530

    photo changed

--- src/components/BoxCard5.vue
+++ src/components/BoxCard5.vue
@@ -1,10 +1,10 @@
 <template>
     <div style="border-radius: 10px; " class="max-w-sm rounded overflow-hidden shadow-lg">
-  <img class="w-full" src="./assets/Untitled.jpeg" alt="Sunset in the mountains" width="100px" height="200px;">
+  <img class="w-full" src="./assets/559221.jpg" alt="Sunset in the mountains" width="100px" height="200px;">
   <div style="background-color: aliceblue; " class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">Dr. Lekshmi V. Nair</div>
+    <div class="font-bold text-xl mb-2">Dr. Kuruvilla Joseph</div>
     <p class="text-gray-700 text-base">
-Dean Student Activity and Outreach    </p>
+      Dean SA, Registrar, IIST    </p>
   </div>
   
   </div>
